cacb30ec760595e8ab24764d43b5cb2e70a838b1
nazonokusa1616 <153412258+nazonokusa1616@users.noreply.github.com>
2023-12-23 00:16:20 +0900

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -12,9 +12,9 @@ var hiduke = new Date();
 hiduke.getFullYear();
 hiduke.getMonth();
 hiduke.getDate();
-var date1 = new Date(2023, 11, 23, 0, 0, 0);
+var date1 = new Date(2024, 0, 6, 0, 0, 0);
 
 if(hiduke>date1){
     window.alert("特別な日を迎えました");
-    window.location.href = './img/17.png';
+    window.location.href = './img/16.png';
 }
